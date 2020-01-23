3e435cd10844379c46d6eb2b08c159f585054ba4
gonjavi <javiercaliescali@gmail.com>
2020-01-23 17:03:38 -0500

    Adding books and displaying them on the table

--- js/script.js
+++ js/script.js
@@ -1,25 +1,40 @@
-let myLibrary = [["uno", "dos", "tres"],
-                 ["uno", "dos", "tres"]];
+let myLibrary = [];
 
-table = document.getElementById("table");
-
-for (var i = 0; i < myLibrary.length; i++){
-  for (var j = 0; j < myLibrary[i].length; j++){
-    table.rows[i].cells[j].innerHTML = myLibrary[i][j];
-  }
-}
-
-
-function Book(author, title, pages) {
+function Book(author, title, pages, price) {
   this.author = author;
   this.title = title;
   this.pages = pages;
+  this.price = price;
 }
 
 function addBookToLibrary() {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
-  myLibrary.push(new Book(title,author,pages));
+  let price = document.getElementById("price").value;
+  myLibrary.push(new Book(title, author, pages, price));
 }
 
+function myFunction(){
+  addBookToLibrary();
+  renderTitles();
+  render();
+}
+
+function renderTitles(){
+
+}
+
+function render(){
+  let displayBook = ""; 
+  
+  for (let book of myLibrary) {
+    displayBook+="<tr>";
+    displayBook+="<td>"+book.title+"</td>";
+    displayBook+="<td>"+book.author+"</td>";
+    displayBook+="<td>"+book.pages+"</td>";
+    displayBook+="<td>"+book.price+"</td>";
+    displayBook+="</tr>";
+  }
+    document.querySelector("tbody").innerHTML = displayBook; 
+}
