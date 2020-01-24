bd6ab3b2888989877e669d8cff29571d5f52af35
gonjavi <javiercaliescali@gmail.com>
2020-01-24 18:44:37 -0500

    creating function to remove a book and update read status

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 let myLibrary = [];
 
-function Book(author, title, pages, price) {
+function Book(author, title, pages, read, price) {
   this.author = author;
   this.title = title;
   this.pages = pages;
+  this.read = read;
   this.price = price;
 }
 
@@ -11,30 +12,56 @@ function addBookToLibrary() {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
+  let checkbox = document.getElementById("read");
   let price = document.getElementById("price").value;
-  myLibrary.push(new Book(title, author, pages, price));
+  let read;
+
+  if(checkbox.checked == true){
+    read = "Yes";
+  }else{
+    read = "No";
+  }
+
+  myLibrary.push(new Book(title, author, pages, read, price));
 }
 
-function myFunction(){
+function myAddBooks(){
   addBookToLibrary();
-  renderTitles();
   render();
 }
 
-function renderTitles(){
-
-}
 
 function render(){
   let displayBook = ""; 
   
   for (let book of myLibrary) {
-    displayBook+="<tr>";
-    displayBook+="<td>"+book.title+"</td>";
-    displayBook+="<td>"+book.author+"</td>";
-    displayBook+="<td>"+book.pages+"</td>";
-    displayBook+="<td>"+book.price+"</td>";
-    displayBook+="</tr>";
+    displayBook += "<tr>";
+    displayBook += "<td>"+book.title+"</td>";
+    displayBook += "<td>"+book.author+"</td>";
+    displayBook += "<td>"+book.pages+"</td>";
+    displayBook += "<td>"+book.read+"</td>";
+    displayBook += "<td>"+book.price+"</td>";
+    displayBook += "<td><button type='button' id='remove' onclick='removeBook("+myLibrary.indexOf(book)+")'>Delete</button></td>";
+    displayBook += "<td><button type='button' id='changeRead' onclick='changeStatus("+myLibrary.indexOf(book)+")'>Change Status</button></td>";
+    displayBook += "</tr>";
   }
     document.querySelector("tbody").innerHTML = displayBook; 
+}
+
+function removeBook(index){
+ myLibrary.splice(index,1);
+ render();
+}
+
+function changeStatus(index){
+  for (let b of myLibrary){
+    if (myLibrary.indexOf(b) === index){
+      if (b.read === "Yes"){
+        b.read = "No";
+      }else{
+        b.read = "Yes";
+      } 
+    }
+  }
+  render();
 }
