98e0866fc0fc782f261edce619c3a5d832d569bf
gonjavi <javiercaliescali@gmail.com>
2020-01-25 10:46:31 -0500

    Adding validation

--- js/script.js
+++ js/script.js
@@ -9,17 +9,27 @@ function Book(author, title, pages, read, price) {
 }
 
 function addBookToLibrary() {
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
-  let checkbox = document.getElementById("read");
-  let price = document.getElementById("price").value;
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let pages = document.getElementById('pages').value;
+  let checkbox = document.getElementById('read');
+  let price = document.getElementById('price').value;
   let read;
 
+  var ok = true;
+  var msg = 'Please enter all the information requested:\n';
+  if(title === "" || author === "" && pages === "" || price === "")
+  {
+    ok = false;
+  }
+   if(ok == false){
+    alert(msg);
+  return ok;
+  }
   if(checkbox.checked == true){
-    read = "Yes";
+    read = 'Yes';
   }else{
-    read = "No";
+    read = 'No';
   }
 
   myLibrary.push(new Book(title, author, pages, read, price));
@@ -28,6 +38,7 @@ function addBookToLibrary() {
 function myAddBooks(){
   addBookToLibrary();
   render();
+  clear();
 }
 
 
@@ -56,12 +67,20 @@ function removeBook(index){
 function changeStatus(index){
   for (let b of myLibrary){
     if (myLibrary.indexOf(b) === index){
-      if (b.read === "Yes"){
-        b.read = "No";
+      if (b.read === 'Yes'){
+        b.read = 'No';
       }else{
-        b.read = "Yes";
+        b.read = 'Yes';
       } 
     }
   }
   render();
+}
+
+function clear(){
+  document.getElementById('title').value = "";
+  document.getElementById('author').value = "";
+  document.getElementById('pages').value = "";
+  document.getElementById('read').checked = false;
+  document.getElementById('price').value = "";
 }
